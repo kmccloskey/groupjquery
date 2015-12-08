7350039266022bf5c37262eb12ca075684e31f80
Kyle McCloskey <mccloskeykyle@gmail.com>
2015-12-08 17:55:13 -0500

    nothing is working as intended

--- js.js
+++ js.js
@@ -6,7 +6,7 @@ alert("are you working?")
 // var passWord = 12345
 // var userName = ("kyle")
 
-$("#password").change(function(){
+$("#password").change(function(pass){
 	if($("#password").val() == ""){
 		alert('password is blank');
 	}else if($("#password").val() === "12345"){
@@ -15,6 +15,7 @@ $("#password").change(function(){
 		alert('password invalid')
 	}
 });
+
 $("#username").change(function(){
 	if($("#username").val() == ""){
 		alert('username is blank');
@@ -24,15 +25,36 @@ $("#username").change(function(){
 		alert('username invalid')
 	}
 });
-});
-$("#date").change(function(){
-	if($("#date").val() == ""){
+$("#date1").change(function(){
+	if($("#date1").val() == ""){
 		alert('date is blank');
-	}else if($("#date").val() === "12/08/2015"){
+	}else if($("#date1").val() === "12"){
 		alert('welcome!');
 	}else{
 		alert('date invalid')
 	}
 });
+$("#date2").change(function(){
+	if($("#date2").val() == ""){
+		alert('date is blank');
+	}else if($("#date2").val() === "08"){
+		alert('welcome!');
+	}else{
+		alert('date invalid')
+	}
+});
+$("#date1").change(function(){
+	if($("#date3").val() == ""){
+		alert('date is <b></b>lank');
+	}else if($("#date3").val() === "2015"){
+		alert('welcome!');
+	}else{
+		alert('date invalid')
+	}
+});
+// $("#num").change(function(num){
+// 	if($("#num").val() === {"a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"; "k"; "l"; "m"; "n"; "o"; "p"; "q"; "r"; "s"; "t"; "u"; "v"; "w"; "x"; "y"; "z"}
+// 		.html("<h1>You entered a letter</h1>")
+// });
 
 });
